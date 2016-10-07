77a4dbb449045dddd6a8224f2777d5a312bc83dd
reveiw <1121773788@qqcom>
2016-10-07 18:57:55 +0800

    format info and change account

--- javascripts/text.js
+++ javascripts/text.js
@@ -1,3 +1,6 @@
+var info = {
+	account: "kanaikan"
+};
 var resume = [
 	"<b class='alter'>== Personal Information ==</b><br>"+
 	"Name: Shikan Chen<br>"+
@@ -12,21 +15,21 @@ var resume = [
 	"<b  class='alter'>== My Projects ==</b><br>"+
 	
 		"<b>Stable Projects</b><br>"+
-		"<a href=\"http://jerrybucc.github.io/Termino_TerminalOL\">TerminalOL</a>: "+"Website version terminal emulator compatible to Linux commands"+"<br>"+
-		"<a href=\"https://github.com/Jerrybucc/Termino_Forum\">Forum</a>: "+"A forum using PHP for server, MySQL for database and bootstrap for designing"+"<br>"+
-		"<a href=\"https://github.com/Jerrybucc/No.100\">No.100</a>: "+"A site for people to post their emotions everyday"+"<br>"+
-		"<a href=\"https://jerrybucc.github.io/Planner\">Planner</a>: "+"A HTML-based tool for chinese students to generate a plan of memorizing vocabulary by The Ebbinghaus Forgetting Curve"+"<br>"+
+		"<a href=\"http://"+info.account+".github.io/Termino_TerminalOL\">TerminalOL</a>: "+"Website version terminal emulator compatible to Linux commands"+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/Termino_Forum\">Forum</a>: "+"A forum using PHP for server, MySQL for database and bootstrap for designing"+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/No.100\">No.100</a>: "+"A site for people to post their emotions everyday"+"<br>"+
+		"<a href=\"https://"+info.account+".github.io/Planner\">Planner</a>: "+"A HTML-based tool for chinese students to generate a plan of memorizing vocabulary by The Ebbinghaus Forgetting Curve"+"<br>"+
 		
 		"<b>Alpha Projects</b><br>"+
-		"<a href=\"https://github.com/Jerrybucc/Utopia\">Utopia</a>: "+"a sublime note-taking app"+"<br>"+
-		"<a href=\"https://github.com/Jerrybucc/UtopiaOL\">UtopiaOL</a>: "+"Online version for <a href=\"https://github.com/Jerrybucc/Utopia\">Utopia</a>"+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/Utopia\">Utopia</a>: "+"a sublime note-taking app"+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/UtopiaOL\">UtopiaOL</a>: "+"Online version for <a href=\"https://github.com/"+info.account+"/Utopia\">Utopia</a>"+"<br>"+
 
 		"<b>Beta Projects</b><br>"+
-		"<a href=\"https://github.com/Jerrybucc/Snake\">Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
-		"<a href=\"https://github.com/Jerrybucc/Chatter\">Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English"+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/Snake\">Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/Chatter\">Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English"+"<br>"+
 
 		"<b>Old/Disposed Projects</b><br>"+
-		"<a href=\"http://jerrybucc.github.io/rootAndAffix/\">RootAndAffix</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"
+		"<a href=\"http://"+info.account+".github.io/rootAndAffix/\">RootAndAffix</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"
 
 		
 	];
