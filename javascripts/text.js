00d7dadca54d12b7fff49e7dac96c0061d32b566
Kanai Chen <kanaichenshikan@outlook.com>
2016-11-15 10:18:11 +0800

    update resume

--- javascripts/text.js
+++ javascripts/text.js
@@ -6,14 +6,13 @@ var resume = [
 	"Name: Shikan Chen<br>"+
 // 	"Preferred Name: Jerry<br>"+
 	"Date of Birth: Nov 12th, 1997<br>"+
-	"<a href=\"http://139.196.114.227\">Personal Storage</a><br>"+
 	"<br>^100"+
 
-	"<b  class='alter'>== Education ==</b><br>"+
+	"<b class='alter'>== Education ==</b><br>"+
 	"High School: "+"Ningbo Foreign Language School<br>"+
 	"<br>^100"+
 
-	"<b  class='alter'>== My Projects ==</b><br>"+
+	"<b class='alter'>== My Projects ==</b><br>"+
 	
 		"<b>Stable Projects</b><br>"+
 		"<a href=\"http://"+info.account+".github.io/Termino_TerminalOL\">TerminalOL</a>: "+"Website version terminal emulator compatible to Linux commands"+"<br>"+
@@ -23,15 +22,16 @@ var resume = [
 		
 		"<b>Alpha Projects</b><br>"+
 		"<a href=\"https://github.com/"+info.account+"/termino_THEME\">termino_THEME</a>: "+"an interesting HTML, CSS, and JS framework for developing Website Projects."+"<br>"+
-		"<a href=\"https://github.com/"+info.account+"/Utopia\">Utopia</a>: "+"a sublime note-taking app"+"<br>"+
-		"<a href=\"https://github.com/"+info.account+"/UtopiaOL\">UtopiaOL</a>: "+"Online version for <a href=\"https://github.com/"+info.account+"/Utopia\">Utopia</a>"+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/Snake\">Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
 
 		"<b>Beta Projects</b><br>"+
-		"<a href=\"https://github.com/"+info.account+"/Snake\">Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
 		"<a href=\"https://github.com/"+info.account+"/Chatter\">Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English"+"<br>"+
 
 		"<b>Old/Disposed Projects</b><br>"+
-		"<a href=\"http://"+info.account+".github.io/rootAndAffix/\">Root&&Affix</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"
+		"<a href=\"http://"+info.account+".github.io/rootAndAffix/\">RA School</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"+
+
+	"<b class='alter'== Links ==</b><br>"+
+		"<a href=\"http://139.196.114.227\">Personal Storage</a><br>"
 
 		
 	];
