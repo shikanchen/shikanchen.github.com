5cd79bf026df1d6434b02ef0a6ae9e7b87cbddbd
System Administrator <kanaichenshikan@outlook.com>
2016-11-16 16:11:44 +0800

    redirect url

--- javascripts/text.js
+++ javascripts/text.js
@@ -15,20 +15,20 @@ var resume = [
 	"<b class='alter'>== My Projects ==</b><br>"+
 	
 		"<b>Stable Projects</b><br>"+
-		"<a href=\"http://"+info.account+".github.io/Termino_TerminalOL\">TerminalOL</a>: "+"Website version terminal emulator compatible to Linux commands"+"<br>"+
+		"<a href=\"./Termino_TerminalOL\">TerminalOL</a>: "+"Website version terminal emulator compatible to Linux commands"+"<br>"+
 		"<a href=\"https://github.com/"+info.account+"/Termino_Forum\">Forum</a>: "+"A forum using PHP for server, MySQL for database and bootstrap for designing"+"<br>"+
 		"<a href=\"https://github.com/"+info.account+"/No.100\">No.100</a>: "+"A site for people to post their emotions everyday"+"<br>"+
-		"<a href=\"https://"+info.account+".github.io/Planner\">Planner</a>: "+"A HTML-based tool for chinese students to generate a plan of memorizing vocabulary by The Ebbinghaus Forgetting Curve"+"<br>"+
+		"<a href=\"./Planner\">Planner</a>: "+"A HTML-based tool for chinese students to generate a plan of memorizing vocabulary by The Ebbinghaus Forgetting Curve"+"<br>"+
 		
 		"<b>Alpha Projects</b><br>"+
-		"<a href=\"https://github.com/"+info.account+"/termino_THEME\">termino_THEME</a>: "+"an interesting HTML, CSS, and JS framework for developing Website Projects."+"<br>"+
-		"<a href=\"https://github.com/"+info.account+"/Snake\">Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
+		"<a href=\"./termino_THEME\">termino_THEME</a>: "+"an interesting HTML, CSS, and JS framework for developing Website Projects."+"<br>"+
+		"<a href=\"./Snake\">Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
 
 		"<b>Beta Projects</b><br>"+
-		"<a href=\"https://github.com/"+info.account+"/Chatter\">Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English"+"<br>"+
+		"<a href=\"./Chatter\">Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English"+"<br>"+
 
 		"<b>Old/Disposed Projects</b><br>"+
-		"<a href=\"http://"+info.account+".github.io/rootAndAffix/\">RA School</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"+
+		"<a href=\"./rootAndAffix/\">RA School</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"+
 
 	"<b class='alter'== Links ==</b><br>"+
 		"<a href=\"http://139.196.114.227\">Personal Storage</a><br>"
