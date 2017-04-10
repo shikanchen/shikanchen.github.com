f79e4b330a3f987c8bbe1c8a1ccd7a97d140df0d
Shikan Chen <kanaichenshikan@outlook.com>
2017-04-10 20:42:01 +0800

    update

--- javascripts/text.js
+++ javascripts/text.js
@@ -4,30 +4,32 @@ var info = {
 var resume = [
 	"<b class='alter'>== Personal Information ==</b><br>"+
 	"Name: Shikan Chen<br>"+
-	"Preferred Name: Gerald<br>"+
+	"Preferred Name: Jerry<br>"+
 	"Date of Birth: Nov 12th, 1997<br>"+
 	"<br>^100"+
 
 	"<b class='alter'>== Education ==</b><br>"+
 	"High School: "+"Ningbo Foreign Language School<br>"+
+	"University: "+"Rensselaer Polytechnic Institute<br>"+
 	"<br>^100"+
 
 	"<b class='alter'>== My Projects ==</b><br>"+
 	
-		"<b>Stable Projects</b><br>"+
+		"<b>Completed</b><br>"+
 		"<a href=\"./Termino_TerminalOL\">TerminalOL</a>: "+"Website version terminal emulator compatible to Linux commands"+"<br>"+
-		"<a href=\"https://github.com/"+info.account+"/Termino_Forum\">Forum</a>: "+"A forum using PHP for server, MySQL for database and bootstrap for designing"+"<br>"+
-		"<a href=\"./No.100\">No.100</a>: "+"A site for people to post their emotions everyday"+"<br>"+
 		"<a href=\"./EPlanner\">EPlanner</a>: "+"A HTML-based tool for chinese students to generate a plan of memorizing vocabulary by The Ebbinghaus Forgetting Curve"+"<br>"+
+		"<a href=\"./No.100\">No.100</a>: "+"A site for people to post their emotions everyday"+"<br>"+
 		
-		"<b>Alpha Projects</b><br>"+
+		"<b>Ongoing</b><br>"+
+		"<a href=\"https://github.com/"+info.account+"/Auto-Snake\">Auto-Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
+		"<a href=\"https://github.com/"+info.account+"/Container.js\">Container.js</a>: "+"JavaScript implementation of Java Containers."+"<br>"+
+
+		"<b>Scratches</b><br>"+
+		"<a href=\"https://github.com/"+info.account+"/DatabaseManager\">DatabaseManager</a>: "+"yet another PHP extension implementing Encapsulation of MySQL."+"<br>"+
 		"<a href=\"https://github.com/"+info.account+"/termino_THEME\">termino_THEME</a>: "+"an interesting HTML, CSS, and JS framework for developing Website Projects."+"<br>"+
 
-		"<b>Beta Projects</b><br>"+
-		"<a href=\"https://github.com/"+info.account+"/Container.js\">Container.js</a>: "+"JavaScript implementation of Java Containers."+"<br>"+
-		"<a href=\"https://github.com/"+info.account+"/Auto-Snake\">Auto-Snake</a>: "+"Snake game with AI written by Java."+"<br>"+
-
-		"<b>Old/Disposed Projects</b><br>"+
+		"<b>Old/Disposed</b><br>"+
+		"<a href=\"https://github.com/"+info.account+"/Termino_Forum\">Forum</a>: "+"A forum using PHP for server, MySQL for database and bootstrap for designing"+"<br>"+
 		"<a href=\"./Root-Affix/\">RA School</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile"+"<br>"+
 		"<a href=\"https://github.com/"+info.account+"/Chatter\">Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English"+"<br>"
 
