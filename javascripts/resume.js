96f77036e797029f8fbc1aa2c8754cd4f7195558
Shikan Chen <github.shikanchen@gmail.com>
2018-01-09 17:37:29 -0500

    reconstruct finished

--- javascripts/resume.js
+++ javascripts/resume.js
@@ -1,30 +1,27 @@
-var fillInResume = function () {
-    // resume lines appending
-    var resume = new ContentManger(NODES.resume);
-    resume.p("<b class='alter'>== Personal INFOrmation ==</b>");
-    resume.p("Name: "+INFO.legal_name);
-    resume.p("Preferred Name: "+INFO.preferred_name);
-    resume.p("Date of Birth: "+INFO.dob);
-    resume.p();
-    resume.p("<b class='alter'>== Education ==</b>");
-    resume.p("High School: "+"Ningbo Foreign Language School");
-    resume.p("University: "+"Rensselaer Polytechnic Institute");
-    resume.p();
-    resume.p("<b class='alter'>== Working Experience ==</b>");
-    resume.p("Huiyou Technology Software Development Lab, China (2017 Summer)");
-    resume.p("Analysis of global blockchain index in scry.INFO, Troy, NY (2017 Winter)");
-    resume.p();
-    resume.p("<b class='alter'>== My Projects ==</b>");
-    resume.p("<b>Completed</b>");
-    resume.p("<a href='Terminal'>Terminal</a>: "+"Website version terminal emulator compatible to Linux commands");
-    resume.p("<a href='EPlanner'>EPlanner</a>: "+"A HTML-based tool for chinese students to generate a plan of memorizing vocabulary by The Ebbinghaus Forgetting Curve");
-    resume.p("<a href='No.100'>No.100</a>: "+"A site for people to post their emotions everyday");
-    resume.p("<b>Ongoing</b>");
-    resume.p("<a href='"+INFO.rep_base+INFO.account+"/Container.js'>Container.js</a>: "+"JavaScript implementation of Java Containers.");
-    resume.p("<b>Scratches</b>");
-    resume.p("<a href='"+INFO.rep_base+INFO.account+"/Auto-Snake'>Auto-Snake</a>: "+"Snake game with AI written by Java.");
-    resume.p("<b>Old/Disposed</b>");
-    resume.p("<a href='"+INFO.rep_base+INFO.account+"/Termino_Forum'>Forum</a>: "+"A forum using PHP for server, MySQL for database and bootstrap for designing");
-    resume.p("<a href='./Root-Affix/'>RA School</a>: "+"A Web App for students to learn the roots and affixes written by Jquery Mobile");
-    resume.p("<a href='"+INFO.rep_base+INFO.account+"/Chatter'>Chatter</a>: "+"A chat robot using Huangji, Simsimi and Tuling API, supporting Chinese and English");
-};
+$(function () {
+    // title part
+    var resume = new ContentManger(NODES.title);
+    resume.p(resume.createNewObj('img', {src:'head.jpg'}));
+    resume.p(resume.createNewObj('h1', {text:INFO.legal_name}));
+    resume.p(resume.createNewObj('h2', {text:INFO.motto}));
+
+    // left info part
+    resume.setTarget(NODES.side_info);
+    resume.addSkill('Programming Language Skills', {'Java':90,'PHP':75,'JavaScript':70,'Python':65});
+    resume.addSkill('Other Skills', {'Linux/Unix':60,'Web Development':80});
+    resume.addSkill('Language', {'Mandarin':95,'English':90});
+
+    // project
+    resume.setTarget(NODES.bookmark);
+    resume.addBookmark('Projects');
+    resume.setTarget(BOOKMARK['Projects']);
+    resume.p(resume.createNewObj('ul',{class:'exp'})).setTarget('.exp');
+
+    resume.addProject('GBIManager','A python extension for calculating global blockchain index, designed during an intern in winter, 2017.');
+    resume.addProject('No.100','Everyday Idea Logger.');
+    resume.addProject('Container.js','JavaScript Plug-in for implementing Container.');
+    resume.addProject('Terminal','A web-based Terminal emulator for Linux.');
+    resume.addProject('EPlanner','A web-based tool for chinese students to generate a plan of memorizing vocabulary.');
+    resume.addProject('Auto-Snake','A snake Game with Auto Path AI.');
+
+});
